77f12e96f40f59a39faaff391b0dd50f00d2c08d
Panupongpnp <131930861+Panupongpnp@users.noreply.github.com>
2024-12-01 00:51:13 +0700

    Add files via upload

---
+++ 8.py
@@ -0,0 +1,34 @@
+money = 1000
+
+moo_sub = 150
+mamaTomyam = 6.50
+PaKaPong = 10
+Kai = 120.25
+Coffee = 15.75
+Water = 15.15
+OokKai = 105
+KhaoSarn = 150
+SortNamManHoi = 18
+CharKhey = 21.50
+KanomPangObNoey = 19
+Water_Solid = 10
+KaiHome_Touy = 120
+Water_brown = 20
+PongShuRost = 10.25
+Pepsi = 29.50
+ChaThai = 11.50
+
+price = (moo_sub + mamaTomyam + PaKaPong + Kai + Coffee
+         + Water + OokKai + KhaoSarn + SortNamManHoi
+         + CharKhey + KanomPangObNoey + Water_Solid
+         + KaiHome_Touy + Water_brown + PongShuRost
+         + Pepsi + ChaThai)
+
+money_Torn = money - (moo_sub + mamaTomyam + PaKaPong + Kai + Coffee
+                      + Water + OokKai + KhaoSarn + SortNamManHoi
+                      + CharKhey + KanomPangObNoey + Water_Solid
+                      + KaiHome_Touy + Water_brown + PongShuRost
+                      + Pepsi + ChaThai)
+
+print(f'เเเMoney Torn is {money_Torn:.2f} Baht.')
+print(f'Price is {price} Baht.')
